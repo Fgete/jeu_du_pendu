d8decfec4179ef3d72ad22122e792440259ad837
Lag <f.gete@ludus-academie.com>
2020-11-18 16:01:17 +0100

    v1.0

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// HEADERS
+#include "define.h"
+
+int main()
+{
+    int gameState = 0;
+
+    while (gameState != -1){
+        switch(gameState){
+        case 0:
+            // MENU
+            MenuTitle(&gameState);
+        break;
+        case 1:
+            // GAME
+            DrawGame();
+            Victory(&gameState);
+        break;
+        case 2:
+            // END
+            EndTitle(&gameState);
+        break;
+        }
+    }
+    Quit();
+}
